7cd98f44bd451776246be3211937fcff001a7e1a
Meshal alotaibi <meshal.saus@gmail.com>
2020-05-04 23:00:06 +0300

    adding

--- Calender.js
+++ Calender.js
@@ -1,33 +1,26 @@
 $(document).ready(function () {
-  var planeTextArr = [];  
-
+  //array
+  var planeTextArr = [];
+  // time 
   const TodayDate = moment().format('MMMM Do YYYY');
-
   $('#navbarDate').text(TodayDate);
-
-
+/////// creating pllnaner 
   var EventContainer = $('#PlannerContainer');
-
-  
-
+  // for loop to start from 9
   for (let hours = 9; hours <= 17; hours++) {
+    // creating row and applied
     let RowDiv = $('<div>');
     RowDiv.addClass('row');  //bootstrap
     RowDiv.addClass('PlannerRow');
-
     RowDiv.attr('hour-index', hours);
-
-   
-
+    // cloum 
     let colTimeDiv = $('<div>');
     colTimeDiv.attr('class', 'col-md-2')
-
     let timeBoxSpan = $('<span>');
     timeBoxSpan.attr('class', 'timeBox');
-
+    // diplaying the time 
     let displayHours = 0;
     let displaShape = "";  //AM OR PM
-
     if (hours > 12) {
       displayHours = hours - 12;
       displaShape = "PM";
@@ -37,37 +30,26 @@ $(document).ready(function () {
       displayHours = hours;
       displaShape = "AM";
     }
-
-
+// the text 
     timeBoxSpan.text(`${displayHours} ${displaShape}`);
-
-
-
+// appening the row 
     EventContainer.append(RowDiv);
     RowDiv.append(colTimeDiv); //2-md
     colTimeDiv.append(timeBoxSpan);
-
-
-
-
-
     let index = hours - 9;
-
     let colInputDiv = $('<div>');
     colInputDiv.addClass('col-md-9');
-
     let dailyPlaneSpan = $('<input>');
     dailyPlaneSpan.attr('id', `input-${index}`);
-    //dailyPlaneSpan.attr('hour-index',index);
     dailyPlaneSpan.attr('type', 'text');
     dailyPlaneSpan.attr('class', 'dailySapn');
+// creating the to get the local the stroge from the index of the input and the store plan
+    let getComment = localStorage.getItem("StoredPlans-" + index)
+    if (getComment != null) {
 
-    let getComment=localStorage.getItem("StoredPlans-"+index) 
-    if(getComment!=null){
-      
-      dailyPlaneSpan.val( getComment); //hello
+      dailyPlaneSpan.val(getComment);
     }
-   
+
     RowDiv.append(colInputDiv);
     colInputDiv.append(dailyPlaneSpan)
 
@@ -77,48 +59,31 @@ $(document).ready(function () {
     let SaveBtn = $('<i>');
     SaveBtn.attr('class', 'fa fa-save SaveIcon');
     SaveBtn.attr('id', `${index}`);
-    // SaveBtn.attr('Save-Id', index);
-
     RowDiv.append(colButtonSaveDiv);
     colButtonSaveDiv.append(SaveBtn);
 
     ChangeRowColor(dailyPlaneSpan, hours);
 
   };
+// saving 
+  $(".SaveIcon").on("click", function () {
 
-$(".SaveIcon").on("click",function(){
+    var CurrentIndex = $(this).attr("id")
 
-  var CurrentIndex=$(this).attr("id")
-  
-    var comment=$(`#input-${CurrentIndex}`).val();
-    planeTextArr.push(comment) ;
-    localStorage.setItem("StoredPlans-"+CurrentIndex, comment);
- 
-  
- 
-})
-
-  $(document).on('click', 'i', function (event) {
-
-    event.preventDefault();
-
-    let CurrentIndex = $(this).attr('Save-Id');
+    var comment = $(`#input-${CurrentIndex}`).val();
+    // will be push it the array 
+    planeTextArr.push(comment);
+    // set the local storge 
+    localStorage.setItem("StoredPlans-" + CurrentIndex, comment);
 
 
-    let inputId = '#input-' + CurrentIndex;
 
-
-    let InputValue = $(inputId).val();
-
-
-    planeTextArr[CurrentIndex] = InputValue;
-    localStorage.setItem("StoredPlans", JSON.stringify(planeTextArr));
-
-  });
+  })
 
 });
 
 let nowHours24 = moment().format('H');
+/// condition for the colour 
 function ChangeRowColor(DivRow, hours) {
 
   if (hours > nowHours24) {
--- README.md
+++ README.md
@@ -9,3 +9,19 @@ the Most Import thing in this homework is for loop and how to loop to create
 row and also to have indexs rigth 
 
 then comes the logic where you need to strart from 9:00 AM
+the most important 
+Geeting the loop 
+ for (let hours = 9; hours <= 17; hours++) {
+    // creating row and applied
+    let RowDiv = $('<div>');
+    RowDiv.addClass('row');  //bootstrap
+    RowDiv.addClass('PlannerRow');
+    RowDiv.attr('hour-index', hours);
+ also the local stroge by index of the text 
+
+    let getComment = localStorage.getItem("StoredPlans-" + index)
+    if (getComment != null) {
+
+      dailyPlaneSpan.val(getComment);
+    }
+
